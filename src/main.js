bf441733d13772c9b87206564eaddede296a87ec
Peter Flanagan <pflanagan@grubhub.com>
2023-02-23 11:46:50 -0500

    working scan and replace

--- src/main.js
+++ src/main.js
@@ -1,48 +1,66 @@
 
+const SELECTORS = {
+  tweets: '[data-testid="tweet"]',
+  tweetLikes: '[data-testid="like"]',
+  tweetLikeText: '[data-testid="app-text-transition-container"] span span',
+  tweetRetweets: undefined,
+  tweetViews: '[role="link"]',
+}
+
 function getTweets() {
-  return $('[data-testid="tweet"]');
+  return $(SELECTORS.tweets);
 }
 
 function getTweetLikeCount(tweet) {
   return parseInt(
     $(tweet)
-      .find('[data-testid="like"]')
+      .find(SELECTORS.tweetLikes)
       .attr('aria-label')
   );
 }
 
 function getTweetViewCount(tweet) {
-  const links = $(tweet).find('[role="link"]');
+  const links = $(tweet).find(SELECTORS.tweetViews);
+  const viewLink = links[links.length - 1]
 
   return parseInt(
-    $(links[links.length - 1])
+    $(viewLink)
       .attr('aria-label')
   );
 }
 
 function replaceTweetLikeCountWithTweetLikePercentage(tweet, percent) {
   $(tweet)
-    .find('[data-testid="like"]')
-    .find('[data-testid="app-text-transition-container"] span span')
+    .find(SELECTORS.tweetLikes)
+    .find(SELECTORS.tweetLikeText)
     .text(`${percent.toFixed(2)}%`);
 }
 
 function scanAndModifyTweets() {
   const tweets = getTweets();
   console.log(tweets);
-  tweets.each(t => {
-    const tweetLikeCount = getTweetLikeCount(t);
-    console.log(tweetLikeCount);
-    const tweetViewCount = getTweetViewCount(t);
-    console.log(tweetViewCount);
-    const percent = 100 * tweetLikeCount / (tweetViewCount + 1);
-    console.log(percent);
-    replaceTweetLikeCountWithTweetLikePercentage(t, percent);
-  })
+  for (let i = 0; i < tweets.length; ++i) {
+    const tweetLikeCount = getTweetLikeCount(tweets[i]);
+    const tweetViewCount = getTweetViewCount(tweets[i]);
+    console.log({ tweetLikeCount, tweetViewCount });
+
+    const likePercent = 100 * tweetLikeCount / (tweetViewCount + 1);
+    console.log({ likePercent });
+
+    replaceTweetLikeCountWithTweetLikePercentage(tweets[i], likePercent);
+  }
 }
+
+// let testTweets;
+// function testOneTweet() {
+//   testTweets = getTweets();
+//   getTweetLikeCount(testTweets[0])
+//   getTweetViewCount(testTweets[0])
+//   replaceTweetLikeCountWithTweetLikePercentage(testTweets[0], 74);
+// }
 
 window.onload = function() {
   setInterval(function(){
     scanAndModifyTweets();
-  }, 100);
+  }, 400);
 }
